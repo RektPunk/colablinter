3696eb032291650753c709dd5c46b52bf77ea862
RektPunk <rektpunk@gmail.com>
2025-12-09 09:19:48 +0900

    [Feature] add core funcs `%%check` and `%%format` (#1)

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
 # colablinter
-Linting and formatting Python code in Google Colab.
+
+[![PyPI version](https://img.shields.io/pypi/v/colablinter.svg)](https://pypi.org/project/colablinter/)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Overview
+
+**`colablinter`** is an **IPython magic command extension** for Jupyter and Google Colab environments.
+
+It integrates the high-speed linter **`ruff`** and import sorter **`isort`** to perform cell-by-cell code quality and formatting checks.
+
+## Magic Commands
+
+| Command | Role | Description |
+| :--- | :--- | :--- |
+| **`%%check`** | **Quality Check** | Displays a linting report. |
+| **`%%format`** | **Format, Sort** | Displays a formatting preview (**for copy/paste**) |
+
+After executing a magic command, the **original code** of the cell is executed (if applicable to the command).
+
+## Installation
+
+Requires Python 3.12 or newer.
+
+```bash
+pip install colablinter
+```
+
+## Usage
+The extension must be explicitly loaded in the notebook session before use.
+
+```python
+%load_ext colablinter
+```
+
+1. Check Code Quality
+
+    Use `%%check` to see linting reports.
+    ```python
+    %%check
+
+    def invalid_code(x):
+        return x + y # 'y' is not defined
+    ```
+
+    Output examples:
+    ```bash
+    --- Code Quality & Style Check Report ---
+    F821 Undefined name `y`
+    --> notebook_cell.py:4:16
+    |
+    2 | def invalid_code(x):
+    3 |     return x + y # 'y' is not defined
+    |                ^
+    |
+
+    Found 1 error.
+    -------------------------------------------
+    ```
+
+2. Format code preview
+
+    `%%format` will display the formatted code, but the cell executes the original code.
+    ```python
+    %%format
+    import sys
+    import os
+    def calculate_long_sum(a,b,c,d,e,f):
+        return (a+b+c)*(d+e+f)  # messy
+    ```
+
+    Output examples:
+    ```python
+    # Formatted Code
+    import os
+    import sys
+    from datetime import datetime
+
+
+    def calculate_long_sum(a, b, c, d, e, f):
+        return (a + b + c) * (d + e + f)  # messy
+    ```
---
+++ src/colablinter/magics.py
@@ -0,0 +1,81 @@
+import subprocess
+import sys
+
+from IPython.core.magic import Magics, line_cell_magic, magics_class
+
+
+def _execute_command(command: str, input_data: str) -> str | None:
+    try:
+        result = subprocess.run(
+            command,
+            input=input_data,
+            shell=True,
+            capture_output=True,
+            text=True,
+            encoding="utf-8",
+            check=False,
+        )
+        if result.stderr:
+            print(
+                f"[ColabLinter: Subprocess Warning/Error]: {result.stderr.strip()}",
+                file=sys.stderr,
+            )
+        return result.stdout.strip()
+    except Exception as e:
+        print(f"[ColabLinter:ERROR] Error running command: {e}")
+        return
+
+
+@magics_class
+class LintMagics(Magics):
+    _FILE_NAME = "notebook_cell.py"
+
+    @line_cell_magic
+    def check(self, line: str, cell: str | None = None) -> str | None:
+        if not cell or not cell.strip():
+            return
+        self.__check(cell)
+        self.__execute(cell)
+
+    @line_cell_magic
+    def format(self, line: str, cell: str | None = None) -> str | None:
+        if not cell or not cell.strip():
+            return
+        self.__format(cell)
+        self.__execute(cell)
+
+    def __check(self, cell: str) -> None:
+        print("---- Code Quality & Style Check Report ----")
+        if report := _execute_command(
+            f"ruff check --stdin-filename={self._FILE_NAME}", input_data=cell
+        ):
+            print(report)
+        else:
+            print("[ColabLinter:INFO] No issues found. Code is clean.")
+        print("-------------------------------------------")
+
+    def __execute(self, cell: str) -> None:
+        try:
+            self.shell.run_cell(cell, silent=False, store_history=True)
+        except Exception as e:
+            print(f"[ColabLinter:ERROR] Code execution failed: {e}")
+
+    def __format(self, cell: str) -> None:
+        _formatted_code: str | None = _execute_command(
+            f"ruff format --stdin-filename={self._FILE_NAME}", input_data=cell
+        )
+        if _formatted_code is None:
+            print("[ColabLinter:ERROR] Formatting failed.")
+            return
+
+        formatted_code: str | None = _execute_command(
+            "isort --profile=black -", input_data=_formatted_code
+        )
+        code_to_print = (
+            formatted_code if formatted_code is not None else _formatted_code
+        )
+        if cell.strip() != code_to_print.strip():
+            print("# Formatted Code")
+            print(formatted_code.strip())
+        else:
+            print("[ColabLinter:INFO] Code already formatted. No changes needed.")
