73fd39f5034744cf9d728aaebb0796a10cc0be2a
RektPunk <rektpunk@gmail.com>
2026-04-04 20:16:49 +0900

    slpit timer and print failuare when not success

--- src/colablinter/magics.py
+++ src/colablinter/magics.py
@@ -1,6 +1,4 @@
-import time
-
-from IPython.core.interactiveshell import ExecutionInfo, ExecutionResult
+from IPython.core.interactiveshell import ExecutionInfo
 from IPython.core.magic import Magics, cell_magic, line_magic, magics_class
 
 from colablinter.command import (
@@ -9,23 +7,7 @@ from colablinter.command import (
     cell_format,
 )
 from colablinter.logger import logger
-
-
-class CellTimer:
-    def __init__(self):
-        self.start_time = None
-
-    def start(self, info: ExecutionInfo | None = None):
-        self.start_time = time.perf_counter()
-
-    def stop(self, result: ExecutionResult | None = None):
-        if self.start_time:
-            duration = time.perf_counter() - self.start_time
-            if result and result.error_in_exec:
-                logger.info(f"\033[91m✘ Failed | {duration:.3f}s\033[0m")
-            else:
-                logger.info(f"\033[92m✔ Done | {duration:.3f}s\033[0m")
-            self.start_time = None
+from colablinter.timer import CellTimer
 
 
 @magics_class
